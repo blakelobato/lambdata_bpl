cf1f0788b98a359acfe6f5f030004866cc95e145
Blake Lobato <blakelobato1@gmail.com>
2020-01-21 23:14:21 -0800

    _init_.py made

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+Lambdata - a collection of ds helper functions
+"""
+
+import pandas as pd
+import numpy as np
+
+#CAP global variables
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50)) #can put arbitrary code in these!
